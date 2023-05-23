bb6e9619853aaa03c5fc901e4a5e335866664b8f
azizi <fouad.azizi@univ-rouen.fr>
2023-05-23 14:08:01 +0200

    premier commit

---
+++ src/test/java/fr/univrouen/stbserver/StbserverApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univrouen.stbserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StbserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
